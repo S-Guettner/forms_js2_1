ae77eade2995ac434dff14de8960c82922c28ba8
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-11 15:42:52 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let headline = document.getElementById("headline");
+
+let changeHeadline = () =>{
+    headline.classList.add("newHeadlineStyle");
+}
+
+let resetHeadline = () =>{
+    headline.classList.remove("newHeadlineStyle")
+}
